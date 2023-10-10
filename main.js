c2c2400f0a826ddf4b410a10fe97341c2df9d605
JoaocarlosMoreira <105492513+JoaocarlosMoreira@users.noreply.github.com>
2023-10-10 11:13:02 -0300

    ok

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSomPom () {
+    document.queryselector('#som_tecla_pom'). play();
+}
+document.queryselector(.'tecla_pom').onclick = TocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play()
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
